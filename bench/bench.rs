8b16b7333f0ab552d3a0a5a70a009e801e379cb8
Özgür Akkurt <oezgurmakkurt@gmail.com>
2023-02-13 02:09:41 +0300

    improve benchmarks

--- bench/bench.rs
+++ bench/bench.rs
@@ -1,12 +1,12 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{BenchmarkId, criterion_group, criterion_main, Criterion};
 #[cfg(feature = "fasthash")]
 use fasthash::murmur3::Hash128_x64;
 #[cfg(feature = "fasthash")]
 use fasthash::FastHash;
+use highway::HighwayHash;
 use std::hash::Hasher;
 use std::io::Cursor;
-
-static SOURCE: &[u8] = b"The quick brown fox jumps over the lazy dog";
+use rand::RngCore;
 
 fn sha1(data: &[u8]) -> [u8; 20] {
     use sha1::{Digest, Sha1};
@@ -17,37 +17,76 @@ fn sha1(data: &[u8]) -> [u8; 20] {
 fn criterion_benchmark(c: &mut Criterion) {
     let mut group = c.benchmark_group("hashes");
 
-    group.bench_function("sha1", |b| b.iter(|| sha1(SOURCE)));
-    group.bench_function("fastmurmur3", |b| b.iter(|| fastmurmur3::hash(SOURCE)));
-    #[cfg(feature = "murmur3c")]
-    group.bench_function("murmur3c", |b| {
-        b.iter(|| fastmurmur3::murmur3c::hash(SOURCE))
-    });
-    #[cfg(feature = "fasthash")]
-    group.bench_function("fasthash", |b| b.iter(|| Hash128_x64::hash(SOURCE)));
-    group.bench_function("murmur3", |b| {
-        b.iter(|| murmur3::murmur3_x64_128(&mut Cursor::new(SOURCE), 0).unwrap())
-    });
-    group.bench_function("twox_hash::Xxh3Hash128", |b| {
-        b.iter(|| {
-            let mut h = twox_hash::xxh3::Hash128::default();
-            h.write(SOURCE);
-            h.finish()
-        })
-    });
-    group.bench_function("twox_hash::Xxh3Hash64", |b| {
-        b.iter(|| {
-            let mut h = twox_hash::xxh3::Hash64::default();
-            h.write(SOURCE);
-            h.finish()
-        })
-    });
-    group.bench_function("xxhash_rust::xxh3_64", |b| {
-        b.iter(|| xxhash_rust::xxh3::xxh3_64(SOURCE))
-    });
-    group.bench_function("xxhash_rust::xxh3_128", |b| {
-        b.iter(|| xxhash_rust::xxh3::xxh3_128(SOURCE))
-    });
+    for size in [16, 20, 32, 40, 64, 70, 128, 130, 256, 260, 512, 520, 1024, 1030, 2048, 2050, 4096, 5500] {
+        let mut rng = rand::thread_rng();
+        let mut buf = vec![0; size];
+        rng.fill_bytes(&mut buf);
+
+        group.bench_with_input(BenchmarkId::new("sha1", size), &size, |b, _size| {
+            b.iter(|| sha1(&buf))
+        });
+
+        group.bench_with_input(BenchmarkId::new("fastmurmur3", size), &size, |b, _size| {
+            b.iter(|| fastmurmur3::hash(&buf))
+        });
+
+        #[cfg(feature = "murmur3c")]
+        group.bench_with_input(BenchmarkId::new("murmur3c", size), &size, |b, _size| {
+            b.iter(|| fastmurmur3::murmur3c::hash(&buf))
+        });
+
+        #[cfg(feature = "fasthash")]
+        group.bench_with_input(BenchmarkId::new("fasthash", size), &size, |b, _size| {
+            b.iter(|| Hash128_x64::hash(&buf))
+        });
+
+        group.bench_with_input(BenchmarkId::new("murmur3", size), &size, |b, _size| {
+            b.iter(|| murmur3::murmur3_x64_128(&mut Cursor::new(&buf), 0))
+        });
+
+        group.bench_with_input(
+            BenchmarkId::new("twox_hash::Xxh3Hash128", size),
+            &size,
+            |b, _size| {
+                b.iter(|| {
+                    let mut h = twox_hash::xxh3::Hash128::default();
+                    h.write(&buf);
+                    h.finish()
+                })
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("twox_hash::Xxh3Hash64", size),
+            &size,
+            |b, _size| {
+                b.iter(|| {
+                    let mut h = twox_hash::xxh3::Hash64::default();
+                    h.write(&buf);
+                    h.finish()
+                })
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("xxhash_rust::xxh3_64", size),
+            &size,
+            |b, _size| b.iter(|| xxhash_rust::xxh3::xxh3_64(&buf)),
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("xxhash_rust::xxh3_128", size),
+            &size,
+            |b, _size| b.iter(|| xxhash_rust::xxh3::xxh3_128(&buf)),
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("highway::HighwayHasher::hash128", size),
+            &size,
+            |b, _size| b.iter(|| highway::HighwayHasher::default().hash128(&buf)),
+        );
+    }
+
     group.finish();
 }
 
